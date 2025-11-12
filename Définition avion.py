3e6fc6fe9cbebc9c34a4b74d43b6a029974e350d
Enzo KH <enzo.khamassi@ipsa.fr>
2025-11-12 09:09:27 +0100

    feat: ajout de la classe Avion

---
+++ Définition avion.py
@@ -0,0 +1,10 @@
+class Avion:
+    def __init__(self, altitude, carburant, vitesse, couleur, id, position):
+        self.altitude = altitude
+        self.carburant = carburant
+        self.vitesse = vitesse
+        self.couleur = couleur
+        self.id = id
+        self.position = position
+
+

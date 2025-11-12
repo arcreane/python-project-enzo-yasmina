b2f510afd0a2963e1b7df03c5368fa7673aead37
Enzo KH <enzo.khamassi@ipsa.fr>
2025-11-12 09:15:34 +0100

    feat: ajout de la classe Avion

--- Définition avion.py
+++ Définition avion.py
@@ -1,10 +1,12 @@
 class Avion:
-    def __init__(self, altitude, carburant, vitesse, couleur, id, position):
-        self.altitude = altitude
-        self.carburant = carburant
-        self.vitesse = vitesse
+    def __init__(self, etat, altitude, carburant, vitesse, couleur, id, position, classe = "inconnu"):
+        self.altitude = altitude # en m
+        self.carburant = carburant # en %
+        self.vitesse = vitesse # en km/h
         self.couleur = couleur
         self.id = id
-        self.position = position
+        self.position = position # définie par (x,y)
+        self.classe = classe #jet, ligne, cargo...
+
 
 

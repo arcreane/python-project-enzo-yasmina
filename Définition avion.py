29f049bc7f8e96937461eef75bfc8cf818726c13
Enzo KH <enzo.khamassi@ipsa.fr>
2025-11-12 10:14:50 +0100

    feat: ajout de la classe Avion

--- Définition avion.py
+++ Définition avion.py
@@ -1,5 +1,5 @@
 class Avion:
-    def __init__(self, etat, altitude, carburant, vitesse, couleur, id, position, classe = "inconnu"):
+    def __init__(self, etat, altitude, carburant, vitesse, couleur, id, position, classe = "inconnu", altitude_limitesup, altitude_limiteinf):
         self.altitude = altitude # en m
         self.carburant = carburant # en %
         self.vitesse = vitesse # en km/h
@@ -8,10 +8,27 @@ class Avion:
         self.position = position # définie par (x,y)
         self.classe = classe #jet, ligne, cargo...
         self.etat = "en vol" #par défaut
+        self.altitude_limitesup = 3000 #à ajuster
+        self.altitude_limiteinf = 1000 #à ajuster
 
-
-    def monter(self, delta):
+    def monter(self, delta = 1000 ):    #delta (en m) à ajuster plus tard
         self.altitude += delta
-        print(f"Altitude actuelle : {self.altitude} m")
+        if self.altitude >= self.altitude_limitesup:
+            self.altitude = self.altitude_limitesup
+            print(f"Altitude : {self.altitude} m. Attention à la limite d'altitude de jeu !")
+        else:
+            print(f"Altitude : {self.altitude} m")
+
+
+
+    def descendre(self, delta = 1000 ):     #delta (en m) à ajuster plus tard
+        self.altitude -= delta
+        if  self.altitude =< self.altitude_limiteinf:
+            self.altitude = self.altitudeinf
+            print(f"Altitude : {self.altitude} m. Attention à la limite d'altitude de jeu!")
+        else:
+            print(f"Altitude : {self.altitude} m")
+
+
 
 

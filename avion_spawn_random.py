b6aecb7d5101055094a7169337a031a1a600449d
Enzo KH <enzo.khamassi@ipsa.fr>
2025-12-07 14:45:48 +0100

    feat : ajout de la piste

--- avion_spawn_random.py
+++ avion_spawn_random.py
@@ -5,35 +5,41 @@ from Bordures import X_MIN, Y_MIN, X_MAX, Y_MAX
 
 ID_COMPTEUR = 0
 
-zones_conflit = [(X_MAX*0.3, Y_MAX*0.5), (X_MAX*0.7, Y_MAX*0.5), (X_MAX*0.9, Y_MAX*0.5)]
-altitudes_conflit = [6000, 7000 ,8000]
-vitesses = [140 ,180, 240, 300]
+zones_conflit = [
+    (X_MAX * 0.3, Y_MAX * 0.5),
+    (X_MAX * 0.7, Y_MAX * 0.5),
+    (X_MAX * 0.9, Y_MAX * 0.5)
+]
+
+altitudes_conflit = [6000, 7000, 8000]
+vitesses = [140, 180, 240, 300]
 
 classes_possibles = ["jet", "ligne", "cargo"]
+couleurs_possibles = ["rouge", "vert", "orange", "noir"]  # ✅ AJOUT IMPORTANT
 
 
 def normal(vx, vy):
     norme = m.sqrt(vx**2 + vy**2)
-    return vx/norme, vy/norme
+    return vx / norme, vy / norme
+
 
 def generer_avion_collision():
     global ID_COMPTEUR
     ID_COMPTEUR += 1
+
     bord = random.choice(["haut", "bas", "gauche", "droite"])
 
     if bord == "haut":
         x, y = random.uniform(X_MIN, X_MAX), Y_MAX
-
     elif bord == "bas":
         x, y = random.uniform(X_MIN, X_MAX), Y_MIN
-
     elif bord == "gauche":
         x, y = X_MIN, random.uniform(Y_MIN, Y_MAX)
-
     else:
         x, y = X_MAX, random.uniform(Y_MIN, Y_MAX)
 
-    position = (x,y)
+    position = (x, y)
+
     cible_x, cible_y = random.choice(zones_conflit)
     dx = cible_x - x
     dy = cible_y - y
@@ -47,6 +53,7 @@ def generer_avion_collision():
     altitude = random.choice(altitudes_conflit)
 
     classe = random.choice(classes_possibles)
+    couleur = random.choice(couleurs_possibles)  # ✅ COULEUR AJOUTÉE
 
     avion = Avion(
         altitude=altitude,
@@ -58,6 +65,8 @@ def generer_avion_collision():
         altitude_limitesup=9000,
         altitude_limiteinf=5000,
         classe=classe,
-        etat="en vol")
+        couleur=couleur,   # ✅ TRANSMISE À AVION
+        etat="en vol"
+    )
 
     return avion
